cada7d53f0d1d5541c1660e61b6ff61a5cc49174
Stanislav22219 <stasukhostavsky@gmail.com>
2025-05-28 18:25:06 +0300

    testing

---
+++ VideoGames/VideoGames.REST/Controllers/GameController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using VideoGames.REST.Models;
+using VideoGames.Common;
+
+namespace VideoGames.REST.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GameController : ControllerBase
+    {
+        private readonly ICrudServiceAsync<GameModel> _gameService;
+
+        public GameController(ICrudServiceAsync<GameModel> gameService)
+        {
+            _gameService = gameService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(GameModel game)
+        {
+            var result = await _gameService.CreateAsync(game);
+            return result ? StatusCode(201) : BadRequest();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Read(Guid id)
+        {
+            var game = await _gameService.ReadAsync(id);
+            return game != null ? Ok(game) : NotFound();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(Guid id, GameModel game)
+        {
+            var result = await _gameService.UpdateAsync(game);
+            return result ? NoContent() : NotFound();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var game = await _gameService.ReadAsync(id);
+            if (game == null) return NotFound();
+
+            var result = await _gameService.RemoveAsync(game);
+            return result ? NoContent() : BadRequest();
+        }
+    }
+}
---
+++ VideoGames/VideoGames.REST/Models/GameModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace VideoGames.REST.Models
+{
+    public class GameModel
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+    }
+}
